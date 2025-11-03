3a2ba1086d91dffb436fbfc4ded4514555a23a1e
AdeyemiCode <abdulquadrimahmud06@gmail.com>
2025-11-03 12:41:29 +0100

    changes made

--- src/app/test/[foodId]/page.jsx
+++ src/app/test/[foodId]/page.jsx
@@ -1,19 +1,29 @@
 "use client";
 
+import { useApi } from "@/app/context/ApiContext";
 import { useParams } from "next/navigation";
 
 export default function FoodTestPage() {
-  const { foodId } = useParams(); // or use foodId, depending on your folder name
-  console.log("Dynamic Route ID:", foodId);
+  const params = useParams();
+
+  const {baseUrl} = useApi();
+
+  console.log("Dynamic Route Params:", params);
+
+  const foodId = params.foodId; // must match folder name
+  
+  useEffect(() => {
+    if (!foodId) return;
+
+    axios.get(`${baseUrl}vendors/foods/get-food?id=${foodId}`).then(res => setFood(res.data));
+  }, [foodId]);
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50">
       <h1 className="text-2xl font-bold text-gray-800">
         Testing Dynamic Route
       </h1>
-      <p className="mt-4 text-gray-600">
-        The ID passed in the URL is:
-      </p>
+      <p className="mt-4 text-gray-600">The ID passed in the URL is:</p>
       <div className="mt-2 px-4 py-2 bg-orange-100 text-orange-700 rounded-md font-mono text-lg">
         {foodId}
       </div>

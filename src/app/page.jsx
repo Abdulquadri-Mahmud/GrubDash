eabae32cd03dc9761149371037e02bf7385dad31
AdeyemiCode <abdulquadrimahmud06@gmail.com>
2025-11-03 08:11:19 +0100

    changes made

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,19 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import AuthLoader from "./components/AuthLoader";
 
-export default function Home() {
+export default function RootPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("userToken");
+    if (token) {
+      router.push("/home");
+    } else {
+      router.push("/auth/signin");
+    }
+  }, [router]);
+
   return <AuthLoader />;
 }

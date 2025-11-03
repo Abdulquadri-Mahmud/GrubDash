b582c238215b871057b45e1c8f71bb7650a22912
AdeyemiCode <abdulquadrimahmud06@gmail.com>
2025-11-03 08:15:37 +0100

    changes made

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,7 @@
 "use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+
 import AuthLoader from "./components/AuthLoader";
 
-export default function RootPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const token = localStorage.getItem("userToken");
-    if (token) {
-      router.push("/home");
-    } else {
-      router.push("/auth/signin");
-    }
-  }, [router]);
-
-  return <AuthLoader/>;
+export default function Home() {
+  return <AuthLoader />;
 }
